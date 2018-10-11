dab0152b16275659bc4df7b7d54c1361570156b4
John David Pressman <jd@jdpressman.com>
2018-10-11 01:32:04 -0700

    Rough draft that uses ElementTree API

--- pmwiki_tables.py
+++ pmwiki_tables.py
@@ -1,20 +1,29 @@
+from markdown.extensions import Extension
 from markdown.inlinepatterns import InlineProcessor
 from markdown.util import etree
+import pdb
 
-TABLE_PATTERN = r'\|\| (.+=\"?.+\"?[\n ])+|(?:\|\|! ([^\|]*))|(?:\|\| ([^\|]*))'
+TABLE_PATTERN = r'\|\| ?([^\|]+=\"?.+\"?)*\n(?:\|\|! ?(.*) ?\|\|!\n)?(?:\|\| ?(.*) ?\|\|\n)+'
+
+class PmWikiTables(Extension):
+    def extendMarkdown(self, md):
+        md.inlinePatterns.register(TablePattern(TABLE_PATTERN), "pmwiki_tables",
+                                   20)
 
 class TablePattern(InlineProcessor):
     def handleMatch(self, m, data):
-        if m.string.startswith("||") and not m.string.strip().endswith("||"):
-            # The escaping and sanitization logic here is a pain so I'll do it
-            # later
-            table = etree.Element('table')
-            return table, m.start(0), m.end(0)
-        if m.string.startswith("||!"):
-            # Match a table header
-            th = etree.Element('th')
-            th.text = m.group().split()[1]
-            return th
-        else:
-            pass
+        # The escaping and sanitization logic here is a pain so I'll do it
+        # later
+        pdb.set_trace()
+        lines = m.string.splitlines()
+        table = etree.Element('table')
+        thead = etree.SubElement(table, "thead")
+        tbody = etree.SubElement(table, "tbody")
+        rows_start = 2 if lines[1].startswith("||!") else 1
+        for line in lines[rows_start:]:
+            tr = etree.SubElement(tbody, 'tr')
+            for data in m.groups()[2].split("||"):
+                td = etree.SubElement(tr, "td")
+                td.text = data.strip()
+        return table, m.start(0), m.end(0)
         
