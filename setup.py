c510403c3aba8f20f021a2aa2bcc89b649e8ab51
John David Pressman <jd@jdpressman.com>
2019-01-13 00:05:56 -0800

    Fix stupid typo where I forgot an =

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="mdx-pmwiki-tables",
     version="0.1",
-    packages=['mdx_pmwiki_tables'],
-    install_requires['Markdown>=2.6.11'],
+    packages=['mdx_pmwiki_tables',],
+    install_requires=['Markdown>=2.6.11'],
     author="John David Pressman",
     author_email="jd@jdpressman.com",
     description=("Markdown extension that implements PmWiki table syntax. See "
