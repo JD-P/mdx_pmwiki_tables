0a3078995a353cb1ccce1423dd6767c8fb252e9c
John David Pressman <jd@jdpressman.com>
2019-01-12 22:56:59 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mdx-pmwiki-tables",
+    version="0.1",
+    install_requires['Markdown>=2.6.11'],
+    author="John David Pressman",
+    author_email="jd@jdpressman.com",
+    description=("Markdown extension that implements PmWiki table syntax. See "
+                 "http://www.pmwiki.org/wiki/PmWiki/Tables for more information."),
+    license="MIT",
+    url="https://github.com/JD-P/mdx_pmwiki_tables")
